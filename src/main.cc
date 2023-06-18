da47680d21ed8d80a6de914ce22e5cf0d6f9b6a0
Gleb Khaykin <khaykingleb@gmail.com>
2023-06-19 02:06:48 +0400

    style: :art:

--- src/main.cc
+++ src/main.cc
@@ -10,41 +10,27 @@ int main(int argc, char *argv[]) {
     using Arguments = std::vector<std::string>;
 
     po::options_description desc("Allowed options");
-
-    desc.add_options()  //
-
-        ("help,h", "Print usage message")  //
-
+    desc.add_options()                                                                     //
+        ("help,h", "Print usage message")                                                  //
         ("compress,e", po::value<Arguments>()->multitoken(), "Compress files to archive")  //
-
         ("decompress,d", po::value<std::string>(), "Decompress archive");
 
     po::variables_map vm;
-
     po::parsed_options parsed = po::command_line_parser(argc, argv).options(desc).run();
-
     po::store(parsed, vm);
-
     po::notify(vm);
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
-
     } else if (vm.count("compress")) {
         Arguments input = vm["compress"].as<Arguments>();
-
         std::string archive_path = input.at(0);
-
         Arguments file_paths = Arguments(input.begin() + 1, input.end());
-
     } else if (vm.count("decompress")) {
         std::string archive_path = vm["decompress"].as<std::string>();
-
     } else {
         std::cout << "Please, specify valid argument. For more information, type `./archiver -h`."
-
                   << std::endl;
     }
-
     return 0;
 }
