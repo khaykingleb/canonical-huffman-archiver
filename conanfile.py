daf65fd12a150f77f09e3291cac356ee23a9df30
Gleb Khaykin <khaykingleb@gmail.com>
2024-05-04 20:39:26 +0400

    style: :art:

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,8 @@
+"""Conan recipe for the Compressor."""
+
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
+
 
 class CompressorRecipe(ConanFile):
     """Conan recipe for the Compressor project."""
@@ -7,21 +10,24 @@ class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
-    def requirements(self):
+    def requirements(self) -> None:
         """Declare the dependencies for the project."""
         self.requires("boost/1.82.0")
         self.requires("gtest/1.13.0")
 
-    def build_requirements(self):
-        """Declare the build requirements for the project. These are tools needed to build the
-        project but aren't needed to use the resulting package.
+    def build_requirements(self) -> None:
+        """Declare the build requirements for the project.
+
+        These are tools needed to build the project but aren't needed to use the resulting package.
         """
         self.build_requires("cmake/3.26.4")
 
-    def layout(self):
-        """Specifies the layout of the package."""
+    def layout(self) -> None:
+        """Specify the layout of the package."""
         cmake_layout(self)
 
-    def validate(self):
-        """Validates the recipe. Raises an error for non-supported configurations."""
-        pass
+    def validate(self) -> None:
+        """Validate the recipe.
+
+        Raises an error for non-supported configurations.
+        """
