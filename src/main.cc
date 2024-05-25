12a9445d07b8cbf1b2dc947b6a3912a623b1a568
Gleb Khaykin <khaykingleb@gmail.com>
2024-05-25 20:59:19 +0400

    feat: support only .huff and .txt extensions

--- src/main.cc
+++ src/main.cc
@@ -29,8 +29,23 @@ int main(int argc, char* argv[])
     else if (vm.count("compress"))
     {
         Arguments input = vm["compress"].as<Arguments>();
+
         std::string archive_path = input.at(0);
+        if (!archive_path.ends_with(".huff"))
+        {
+            std::cout << "Archive file should have .huff extension." << std::endl;
+            return 1;
+        }
+
         Arguments file_paths = Arguments(input.begin() + 1, input.end());
+        for (const auto& file_path : file_paths)
+        {
+            if (!file_path.ends_with(".txt"))
+            {
+                std::cout << "Only .txt files are supported." << std::endl;
+                return 1;
+            }
+        }
 
         Archiver archiver(archive_path, file_paths);
         archiver.Compress();
@@ -38,6 +53,11 @@ int main(int argc, char* argv[])
     else if (vm.count("decompress"))
     {
         std::string archive_path = vm["decompress"].as<std::string>();
+        if (!archive_path.ends_with(".huff"))
+        {
+            std::cout << "Archive file should have .huff extension." << std::endl;
+            return 1;
+        }
 
         Archiver archiver(archive_path);
         archiver.Decompress();
