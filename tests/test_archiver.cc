ab88800f8acf0c1b27bc24b0a55faa9809d41f35
Gleb Khaykin <khaykingleb@gmail.com>
2024-05-25 21:14:09 +0400

    test: compression and decompression of several files

--- tests/test_archiver.cc
+++ tests/test_archiver.cc
@@ -3,11 +3,11 @@
 
 #include <gtest/gtest.h>
 
-TEST(ArchiverTest, CompressionAndDecompression)
+TEST(ArchiverTest, CompressionAndDecompressionOfOneFile)
 {
     std::string original_file_text;
     {
-        FileReader reader("test.txt");
+        FileReader reader("test_1.txt");
         while (reader.HasMoreCharacters())
         {
             auto character = reader.ReadCharacter();
@@ -19,7 +19,7 @@ TEST(ArchiverTest, CompressionAndDecompression)
     }
 
     std::string archive_path = "test_archive.huff";
-    std::vector<std::string> file_paths = { "test.txt" };
+    std::vector<std::string> file_paths = { "test_1.txt" };
     Archiver archiver(archive_path, file_paths);
 
     archiver.Compress();
@@ -27,7 +27,7 @@ TEST(ArchiverTest, CompressionAndDecompression)
 
     std::string decompressed_file_text;
     {
-        FileReader reader("test.txt");
+        FileReader reader("test_1.txt");
         while (reader.HasMoreCharacters())
         {
             auto character = reader.ReadCharacter();
@@ -39,4 +39,70 @@ TEST(ArchiverTest, CompressionAndDecompression)
     }
 
     EXPECT_EQ(original_file_text, decompressed_file_text);
+}
+
+TEST(ArchiverTest, CompressionAndDecompressionOfSeveralFiles)
+{
+    std::string original_file_text_1;
+    {
+        FileReader reader("test_1.txt");
+        while (reader.HasMoreCharacters())
+        {
+            auto character = reader.ReadCharacter();
+            if (character.has_value())
+            {
+                original_file_text_1.push_back(character.value());
+            }
+        }
+    }
+
+    std::string original_file_text_2;
+    {
+        FileReader reader("test_2.txt");
+        while (reader.HasMoreCharacters())
+        {
+            auto character = reader.ReadCharacter();
+            if (character.has_value())
+            {
+                original_file_text_2.push_back(character.value());
+            }
+        }
+    }
+
+    std::string archive_path = "test_archive.huff";
+    std::vector<std::string> file_paths = { "test_1.txt", "test_2.txt" };
+    Archiver archiver(archive_path, file_paths);
+
+    archiver.Compress();
+    archiver.Decompress();
+
+    std::string decompressed_file_text;
+    {
+        FileReader reader("test_1.txt");
+        while (reader.HasMoreCharacters())
+        {
+            auto character = reader.ReadCharacter();
+            if (character.has_value())
+            {
+                decompressed_file_text.push_back(character.value());
+            }
+        }
+    }
+
+    EXPECT_EQ(original_file_text_1, decompressed_file_text);
+
+    std::string decompressed_file_text_2;
+    {
+        FileReader reader("test_2.txt");
+        while (reader.HasMoreCharacters())
+        {
+            auto character = reader.ReadCharacter();
+            if (character.has_value())
+            {
+                decompressed_file_text_2.push_back(character.value());
+            }
+        }
+    }
+
+    EXPECT_EQ(original_file_text_2, decompressed_file_text_2);
 }
